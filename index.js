8b9f4b4e3d167ca20091358cf6767b178ddaffcd
Rachel <yarkony.rachel@gmail.com>
2022-02-22 16:00:16 -0600

    files created, started index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+//
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("generateHTML");
+const { userInfo } = require("os");
+
+const teamPosition = [];
+
+// prompt for position
+const promptPosition = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "position",
+      message: "What is the title of the employee you wish to add?",
+      choices: ["Engineer", "Intern", "Add new Title"],
+    },
+  ]);
+};
+
+// Prompt for additional information
+
+const promptAdditionalInfo = (position) => {
+  if (position === "Generate Team Page") {
+    writeHTMLtoFile(teamPosition);
+  } else {
+    const questions = workerQuestions(position);
+    inquirer.prompt(questions).then((userInfo) => {
+      userInfo.position = position;
+      teamPosition.push(userInfo);
+      addNewEmployee();
+    });
+  }
+};
+
+const addNewEmployee = () 
